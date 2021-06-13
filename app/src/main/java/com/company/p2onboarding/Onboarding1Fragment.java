707436326adac8fd14c28b384fca803f594fee38
monkeylucky1551 <80496624+monkeylucky1551@users.noreply.github.com>
2021-06-13 18:32:02 -0500

    ya funciona el onboard

---
+++ app/src/main/java/com/company/p2onboarding/Onboarding1Fragment.java
@@ -0,0 +1,73 @@
+package com.company.p2onboarding;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class Onboarding1Fragment extends Fragment {
+
+    Button botonSiguiente;
+    NavController navController;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_onboarding1, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
+
+        botonSiguiente = view.findViewById(R.id.botonSiguiente);
+
+        botonSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_onboarding1Fragment_to_onboarding2Fragment);
+            }
+        });
+    }
+}
+
+/*
+
+// Con ViewBinding
+public class Onboarding1Fragment extends Fragment {
+
+    private FragmentOnboarding1Binding binding;
+    NavController navController;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentOnboarding1Binding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
+
+        binding.botonSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_onboarding1Fragment_to_onboarding2Fragment);
+            }
+        });
+    }
+}
+*/
---
+++ app/src/main/java/com/company/p2onboarding/Onboarding2Fragment.java
@@ -0,0 +1,75 @@
+package com.company.p2onboarding;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class Onboarding2Fragment extends Fragment {
+
+    Button botonFinalizar;
+    NavController navController;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_onboarding2, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
+
+        botonFinalizar = view.findViewById(R.id.botonFinalizar);
+
+        botonFinalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Onboarding2Fragment.this.getActivity().getBaseContext(), activity_principal.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
+/*
+
+// Con ViewBinding
+public class Onboarding2Fragment extends Fragment {
+
+    private FragmentOnboarding2Binding binding;
+    NavController navController;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentOnboarding2Binding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
+
+        binding.botonFinalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_onboarding2Fragment_to_homeFragment);
+            }
+        });
+    }
+}
+*/
