217a95bf342107d2bab5b909b28d06ed0b6d319f
monkeylucky1551 <80496624+monkeylucky1551@users.noreply.github.com>
2021-06-18 13:37:24 -0500

    - se añadio la funcion de registrarse y ya esta funcionando con Firebase

--- app/src/main/java/com/company/p2onboarding/RegistrarseActivity.java
+++ app/src/main/java/com/company/p2onboarding/RegistrarseActivity.java
@@ -1,14 +1,67 @@
 package com.company.p2onboarding;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegistrarseActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
+    private EditText correo;
+    private EditText contrasena;
+    private EditText contrasenaConfirmacion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrarse);
+        mAuth = FirebaseAuth.getInstance();
+        correo = findViewById(R.id.correo);
+        contrasena = findViewById(R.id.contrasena);
+        contrasenaConfirmacion = findViewById(R.id.contrasenaConfirmacion);
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+    public void registrarUsuario(View view){
+        if(contrasena.getText().toString().equals(contrasenaConfirmacion.getText().toString())){
+            mAuth.createUserWithEmailAndPassword(correo.getText().toString(),contrasena.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                //Log.d(TAG, "signInWithCustomToken:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Toast.makeText(getApplicationContext(),"Usuario creado",Toast.LENGTH_LONG).show();
+                                Intent i = new Intent(getApplicationContext(),MainActivity2.class);
+                                startActivity(i);
+                                //updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                //Log.w(TAG, "signInWithCustomToken:failure", task.getException());
+                                Toast.makeText(getApplicationContext(),"Authentication failed.",Toast.LENGTH_SHORT).show();
+                                //updateUI(null);
+                            }
+                        }
+                    });
+        }
+        else{
+            Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+        }
     }
 }
