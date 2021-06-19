c15bc8f6d4ac1621f18373ea9d841c61ac557beb
monkeylucky1551 <80496624+monkeylucky1551@users.noreply.github.com>
2021-06-18 20:39:00 -0500

    ya no se muestra el tutorial si el usuario ya inicio sesion

--- app/src/main/java/com/company/p2onboarding/MainActivity.java
+++ app/src/main/java/com/company/p2onboarding/MainActivity.java
@@ -3,16 +3,30 @@ package com.company.p2onboarding;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
-    
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
 
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if(mAuth.getCurrentUser()!=null){
+            startActivity(new Intent(MainActivity.this,HomeAppActivity.class));
+            finish();
+        }
     }
 }
