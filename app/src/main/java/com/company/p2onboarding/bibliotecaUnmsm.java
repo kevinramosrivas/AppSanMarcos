460475d325fc8523b681a64902d836775d45ecb6
monkeylucky1551 <80496624+monkeylucky1551@users.noreply.github.com>
2021-07-26 18:15:06 -0500

    - se añadio la opcion texto a voz en el bibliotecaUnmsm.java - se añadieron algunas tildes al activity_home_app.xml

--- app/src/main/java/com/company/p2onboarding/bibliotecaUnmsm.java
+++ app/src/main/java/com/company/p2onboarding/bibliotecaUnmsm.java
@@ -3,11 +3,22 @@ package com.company.p2onboarding;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.ViewFlipper;
 
-public class bibliotecaUnmsm extends AppCompatActivity {
+import java.util.Locale;
+
+public class bibliotecaUnmsm extends AppCompatActivity implements TextToSpeech.OnInitListener{
     ViewFlipper v_flipper;
+    private TextToSpeech tts;
+    private TextView textViewBiblioteca;
+    private ImageButton btnBibliotecaPlay;
+    private ImageButton btnBibliotecaStop;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +30,27 @@ public class bibliotecaUnmsm extends AppCompatActivity {
         for(int image:images){
             flipperImages(image);
         }
+
+
+        tts = new TextToSpeech(this,this);
+        btnBibliotecaPlay = (ImageButton) findViewById(R.id.bibliotecaAVozPlay);
+        btnBibliotecaStop = (ImageButton) findViewById(R.id.bibliotecaAVozStop);
+        textViewBiblioteca = (TextView) findViewById(R.id.textViewBiblioteca);
+        btnBibliotecaPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speakOut();
+            }
+
+
+        });
+        btnBibliotecaStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tts.stop();
+            }
+        });
+
     }
     public void flipperImages(int image){
         ImageView imageView = new ImageView(this);
@@ -29,5 +61,26 @@ public class bibliotecaUnmsm extends AppCompatActivity {
         v_flipper.setInAnimation(this,android.R.anim.slide_out_right);
         v_flipper.setInAnimation(this,android.R.anim.slide_in_left);
 
+    }
+
+    @Override
+    public void onInit(int status) {
+        if(status == TextToSpeech.SUCCESS){
+            int result = tts.setLanguage(Locale.getDefault());
+            if(result==TextToSpeech.LANG_NOT_SUPPORTED
+                    || result ==TextToSpeech.LANG_MISSING_DATA){
+                Log.e("TTS","Este lenguaje no es soportado");
+            }
+            else{
+                speakOut();
+            }
+        }
+        else{
+            Log.e("TTS","inicializacion del lenguaje falla");
+        }
+    }
+    private void speakOut() {
+        String textoEstadio = textViewBiblioteca.getText().toString();
+        tts.speak(textoEstadio,TextToSpeech.QUEUE_FLUSH,null);
     }
 }
