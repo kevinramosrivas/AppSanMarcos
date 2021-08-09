00ac90f5a3a0e520574d43be1eae084a0a99cd13
monkeylucky1551 <80496624+monkeylucky1551@users.noreply.github.com>
2021-08-09 15:06:39 -0500

    - se creo el activity cronologia - se esta añadiendo los textos y titulos en strings.xml

--- app/src/main/java/com/company/p2onboarding/cultura_activity.java
+++ app/src/main/java/com/company/p2onboarding/cultura_activity.java
@@ -11,16 +11,15 @@ import android.widget.ViewFlipper;
 public class cultura_activity extends AppCompatActivity {
 
     ViewFlipper v_flipper;
-
+    TextView textoImagenes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cultura);
 
         int[] images = {R.drawable.cultura_1,R.drawable.cultura_2,R.drawable.cultura_3,R.drawable.cultura_4,R.drawable.cultura_5,R.drawable.cultura_6,R.drawable.cultura_7,R.drawable.cultura_8,R.drawable.cultura_9,R.drawable.cultura_10};
-        //String[] text = {"Bienvenida de cachimbos","Examen Medico","Comedor Universitario","Feria Vocacional","Almuerzo Especial","Presentaciones UNMSM","Banda musical Tuna","Teatro Ella Dunbar","Clubes de Deporte","Grupo Sikuris"};
+        String[] text = {"Bienvenida de cachimbos","Examen Medico","Comedor Universitario","Feria Vocacional","Almuerzo Especial","Presentaciones UNMSM","Banda musical Tuna","Teatro Ella Dunbar","Clubes de Deporte","Grupo Sikuris"};
         v_flipper = (ViewFlipper)findViewById(R.id.flipper_cultura);
-
         for(int image:images){
             flipperImages(image);
         }
