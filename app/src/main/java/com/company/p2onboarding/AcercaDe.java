594bc5ba45c631810a1dbb8176ab4282a24b8e4b
monkeylucky1551 <80496624+monkeylucky1551@users.noreply.github.com>
2021-08-14 21:41:16 -0500

    - se mejoraro la visualizacion de algunas activity(trabajo en proceso) - se cambio el apartado de quienes somos

--- app/src/main/java/com/company/p2onboarding/AcercaDe.java
+++ app/src/main/java/com/company/p2onboarding/AcercaDe.java
@@ -7,15 +7,18 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class AcercaDe extends AppCompatActivity {
     private Button btnDonar;
+    private TextView github;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acerca_de);
 
         btnDonar = (Button)findViewById(R.id.boton_donar);
+        github = (TextView)findViewById(R.id.TextViewGitHub);
         btnDonar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,6 +27,15 @@ public class AcercaDe extends AppCompatActivity {
                 startActivity(intentd);
             }
         });
+        github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://github.com/monkeylucky1551/AppSanMarcos.git");
+                Intent intentd = new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intentd);
+            }
+        });
+
 
     }
 }
